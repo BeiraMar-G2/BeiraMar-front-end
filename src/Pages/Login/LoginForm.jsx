cb36f6ac4bf6c145052f7c90e45f76c119f0d25b
DiegoCampos2507 <diego.campos@sptech.school>
2025-12-06 11:16:35 -0300

    teste ci

--- src/Pages/Login/LoginForm.jsx
+++ src/Pages/Login/LoginForm.jsx
@@ -23,7 +23,7 @@ export function LoginForm() {
   const navigate = useNavigate();
 
   function validarLogin() {
-    api.post("/api/autenticacoes/login", {
+    api.post("/autenticacoes/login", {
       email: email,
       senha: senha
     })
